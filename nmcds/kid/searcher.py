fa41f2f2bf9855491327248428c8125eee62ef8e
sskeerthi54 <sskeerthi54@gmail.com>
2025-07-26 19:18:25 +0530

    Update searcher.py

---
+++ nmcds/kid/searcher.py
@@ -0,0 +1,24 @@
+class WordSearcher:
+    def __init__(self, matrix: list[list[str]]):
+        self.matrix = matrix
+
+    def search_word(self, word: str) -> list[tuple[int, int]]:
+        word = word.lower()
+        positions = []
+
+        for row_idx, row in enumerate(self.matrix):
+            for col_idx, cell in enumerate(row):
+                if cell.lower() == word:
+                    positions.append((row_idx, col_idx))
+        
+        return positions
+#example
+matrix = [
+    ["Hello", "World"],
+    ["Hi" , "hello"],
+    ["HELLO", "Python"]
+]
+
+searcher = WordSearcher(matrix)
+result = searcher.search_word("hello")
+print("Positions found:",result)
